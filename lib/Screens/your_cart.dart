b18076d1a9e9a321bbe26e3001adf55b03bf7e36
Naimul13 <shimon@graduate.utm.my>
2022-06-08 16:05:39 +0600

    cart details updating

---
+++ lib/Screens/your_cart.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:meditech_rent/Screens/add_rent_detail.dart';
+import 'package:meditech_rent/centralized.dart';
+
+class YourCart extends StatefulWidget {
+  String startDate;
+  String endDate;
+  String title;
+  String rent;
+  String image;
+
+  YourCart(this.startDate,this.endDate,this.title, this.rent, this.image);
+  @override
+  State<YourCart> createState() => _YourCartState();
+}
+
+class _YourCartState extends State<YourCart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: white,
+        title: const BoldFont("Your Cart", 20, darkblue),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_outlined,
+            color: darkblue,
+            size: 30,
+          ),
+        ),
+        elevation: 0.0,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: <Color>[
+              Colors.white24,
+              Color(0xFFCAEEFF),
+            ],
+            end: Alignment.bottomCenter,
+            begin: Alignment.topCenter,
+          ),
+        ),
