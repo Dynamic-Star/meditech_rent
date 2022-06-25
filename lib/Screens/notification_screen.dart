7bf8ad88afc0ec3895e81baf6abf3bf0d91f6015
Naimul13 <shimon@graduate.utm.my>
2022-06-25 22:13:04 +0600

    half edited

---
+++ lib/Screens/notification_screen.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:meditech_rent/phone_bloc/notificationservice/local_notification_service.dart';
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // 1. This method call when app in terminated state and you get a notification
+    // when you click on notification app open from terminated state and you can get notification data in this method
+
+    FirebaseMessaging.instance.getInitialMessage().then(
